a2e2e2624c02a54b7394fc33127c6f220b15dd25
Othman Rhajbal <othman.rhajbal00@gmail.com>
2024-05-03 18:05:24 -0700

    AirBnb clone v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,36 @@
 #!/usr/bin/python3
-""""Fabric script that distributes an archive to web servers"""
-
+"""
+Fabric script method:
+    do_deploy: deploys archive to webservers
+Usage:
+    fab -f 2-do_deploy_web_static.py
+    do_deploy:archive_path=versions/web_static_20170315003959.tgz
+    -i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import env, put, run
 import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
-
-env.hosts = ['54.157.144.57', '3.85.148.165']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/alx_server'
+env.hosts = ['35.229.54.225', '35.231.225.251']
 
 
 def do_deploy(archive_path):
-    """ distribute archive to a web server"""
-    if (os.path.isfile(archive_path) is False):
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
         return False
-
     try:
-        new_comp = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
-
-        # push archive to the /tmp/ directory of the web server
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
         put(archive_path, "/tmp/")
-
-        # Uncompress the archive to the folder /data/web_static/releases/<archive filename
-        # without extension> on the web server
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
-
-        # delete the archive created from the web server
-        run("sudo rm /tmp/{}".format(new_comp))
-
-	# move contents into host web_static
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-
-        # remove extra  web_static dir
-        run("sudo rm -rf {}/web_static".format(new_folder))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run('sudo rm -rf /data/web_static/current')
-
-        # Create a new the symbolic link /data/web_static/current on the web server,
-        # linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
-
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
     except:
         return False
